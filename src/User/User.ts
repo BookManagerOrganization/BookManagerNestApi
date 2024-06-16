80f79553c20249c5e77e74f54ce022f5007d2dfb
Tiago Andrade <90466869+ehotigas@users.noreply.github.com>
2024-06-16 18:28:09 -0300

    new: app

---
+++ src/User/User.ts
@@ -0,0 +1,11 @@
+import { Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+export type UserDocument = HydratedDocument<User>
+
+@Schema()
+export class User {
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
